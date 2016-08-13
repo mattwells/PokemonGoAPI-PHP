5f8873c04e43ffa61d886984ae6ece2699a0e6af
Matt Wells <matt@cantseethecode.com>
2016-08-13 20:03:59 +0100

    Pokemon levels unit test

--- tests/CombatPointsCalculatorTest.php
+++ tests/CombatPointsCalculatorTest.php
@@ -7,9 +7,48 @@ class CombatPointsCalculatorTest extends TestCase
     /** @test */
     function level()
     {
-        //
+        $this->assertEquals( 1, Calculator::getLevel(0.094000));
+        $this->assertEquals( 2, Calculator::getLevel(0.166398));
+        $this->assertEquals( 3, Calculator::getLevel(0.215732));
+        $this->assertEquals( 4, Calculator::getLevel(0.255720));
+        $this->assertEquals( 5, Calculator::getLevel(0.290250));
+        $this->assertEquals( 6, Calculator::getLevel(0.321088));
+        $this->assertEquals( 7, Calculator::getLevel(0.349213));
+        $this->assertEquals( 8, Calculator::getLevel(0.375236));
+        $this->assertEquals( 9, Calculator::getLevel(0.399567));
+        $this->assertEquals(10, Calculator::getLevel(0.422500));
+        $this->assertEquals(11, Calculator::getLevel(0.443108));
+        $this->assertEquals(12, Calculator::getLevel(0.462798));
+        $this->assertEquals(13, Calculator::getLevel(0.481685));
+        $this->assertEquals(14, Calculator::getLevel(0.499858));
+        $this->assertEquals(15, Calculator::getLevel(0.517394));
+        $this->assertEquals(16, Calculator::getLevel(0.534354));
+        $this->assertEquals(17, Calculator::getLevel(0.550793));
+        $this->assertEquals(18, Calculator::getLevel(0.566755));
+        $this->assertEquals(19, Calculator::getLevel(0.582279));
+        $this->assertEquals(20, Calculator::getLevel(0.597400));
+        $this->assertEquals(21, Calculator::getLevel(0.612157));
+        $this->assertEquals(22, Calculator::getLevel(0.626567));
+        $this->assertEquals(23, Calculator::getLevel(0.640653));
+        $this->assertEquals(24, Calculator::getLevel(0.654436));
+        $this->assertEquals(25, Calculator::getLevel(0.667934));
+        $this->assertEquals(26, Calculator::getLevel(0.681165));
+        $this->assertEquals(27, Calculator::getLevel(0.694144));
+        $this->assertEquals(28, Calculator::getLevel(0.706884));
+        $this->assertEquals(29, Calculator::getLevel(0.719399));
+        $this->assertEquals(30, Calculator::getLevel(0.731700));
+        $this->assertEquals(31, Calculator::getLevel(0.737769));
+        $this->assertEquals(32, Calculator::getLevel(0.743789));
+        $this->assertEquals(33, Calculator::getLevel(0.749761));
+        $this->assertEquals(34, Calculator::getLevel(0.755686));
+        $this->assertEquals(35, Calculator::getLevel(0.761564));
+        $this->assertEquals(36, Calculator::getLevel(0.767397));
+        $this->assertEquals(37, Calculator::getLevel(0.773187));
+        $this->assertEquals(38, Calculator::getLevel(0.778933));
+        $this->assertEquals(39, Calculator::getLevel(0.784637));
+        $this->assertEquals(40, Calculator::getLevel(0.790300));
     }
-    
+
     /** @test */
     function startdust_cost_for_powerup()
     {
